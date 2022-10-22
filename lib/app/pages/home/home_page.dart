997b7fef95ccfecd485a0109607ff9b77efc2af7
Rafael R. de Oliveira <consultoria@tekplus.com.br>
2022-10-22 12:33:31 -0300

    Sticker dashboard has implemented

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fwc_album_app/app/core/ui/styles/button_styles.dart';
+import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
+import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
+import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
+import 'package:fwc_album_app/app/core/ui/widgets/status_tile.dart';
+import 'package:fwc_album_app/app/core/ui/widgets/sticker_percent.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,18 +12,95 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: context.colors.primary,
       appBar: AppBar(
-        title: const Text('Home Page'),
+        elevation: 0,
+        backgroundColor: context.colors.primary,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            final sp = await SharedPreferences.getInstance();
-            await sp.clear();
-            Navigator.of(context)
-                .pushNamedAndRemoveUntil('/', (route) => false);
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/background.png',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: LayoutBuilder(
+          builder: (_, constrains) {
+            return ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constrains.maxHeight),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 35),
+                        width: MediaQuery.of(context).size.height,
+                        child: Image.asset(
+                          'assets/images/bola.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const StickerPercent(percent: 60),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        '45 figurinhas',
+                        style: context.textStyles.titleWhite,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      StatusTile(
+                        icon: Image.asset('assets/images/all_icon.png'),
+                        label: 'Todas',
+                        value: 45,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      StatusTile(
+                        icon: Image.asset('assets/images/missing_icon.png'),
+                        label: 'Faltando',
+                        value: 500,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      StatusTile(
+                        icon: Image.asset('assets/images/repeated_icon.png'),
+                        label: 'Repetidas',
+                        value: 22,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Button(
+                        onPressed: () {},
+                        width: MediaQuery.of(context).size.width * .9,
+                        style: context.buttonStyles.yellowOutlineButton,
+                        labelStyle: context
+                            .textStyles.textSecondaryFontExtraBold
+                            .copyWith(color: context.colors.yellow),
+                        label: 'Minhas Figurinhas',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
           },
-          child: const Text('Logout'),
         ),
       ),
     );
